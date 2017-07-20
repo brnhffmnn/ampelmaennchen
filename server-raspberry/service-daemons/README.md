cb063daec32a2d409a859b9a39c41f0da5d79ee4
Brian Hoffmann <panzerfahrer@users.noreply.github.com>
2017-07-20 12:24:08 +0200

    Add daemon scripts section

---
+++ server-raspberry/service-daemons/README.md
@@ -0,0 +1,15 @@
+# Collection of platform-specific service daemon scripts
+
+Use these scripts to run the server as a service daemon on your system.
+
+Also provided are scripts to install the services. If you don't trust the scripts (or just want to do it your way) have a look at the install scripts and adapt your setup accordingly.
+
+## Available scripts
+
+### Debian
+- Daemon script: [debian](debian)
+- Install script: [debian-install.sh](debian-install.sh)
+
+```
+wget -O - https://raw.githubusercontent.com/panzerfahrer/ampelmaennchen/master/server-raspberry/service-daemons/debian-install.sh | sudo sh
+```
---
+++ server-raspberry/service-daemons/debian-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+WGET=`which wget`
+GITHUB_BASE=https://raw.githubusercontent.com/panzerfahrer/ampelmaennchen/master/server-raspberry
+
+echo "Downloading service script"
+WGET "$GITHUB_BASE/service-daemons/debian" -O /etc/init.d/ampelmaennchen
+
+echo "Downloading default configuration"
+WGET "$GITHUB_BASE/application.conf" -O /etc/default/ampelmaennchen/application.conf
+
+echo "Preparing application directory"
+mkdir /usr/local/ampelmaennchen
+
+chmod +x /etc/init.d/ampelmaennchen
+update-rc.d ampelmaennchen defaults
+
+echo "Done."
+echo ""
+echo "Tasks to do next:"
+echo " - edit the configuration in '/etc/default/ampelmaennchen/application.conf' "
+echo " - place or link the compiled application in '/usr/local/ampelmaennchen/application.jar' "
+echo ""
+echo "Finally, run 'service ampelmaennchen start' to start the server."
